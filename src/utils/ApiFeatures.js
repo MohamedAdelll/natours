7d89d371c26de1710849bc3afe09154228353b62
Mohamed Ashour <97407829+MohamedAdelll@users.noreply.github.com>
2024-10-29 20:21:30 +0300

    fixing bugs

--- src/utils/ApiFeatures.js
+++ src/utils/ApiFeatures.js
@@ -1,18 +1,18 @@
 module.exports = class ApiFeatures {
-  queryStr;
+  queryParams;
   query;
-  constructor(query, queryStr) {
+  excludedFilter = ['sort', 'page', 'limit', 'fields'];
+  constructor(query, queryParams) {
     this.query = query;
-    this.queryStr = queryStr;
+    this.queryParams = queryParams;
   }
   filter() {
-    const queryStrCopy = { ...this.queryStr };
-    const excludedFilter = ['sort', 'page', 'limit', 'fields'];
-    excludedFilter.forEach((el) => delete queryStrCopy[el]);
+    const newQueryParam = { ...this.queryParams };
+    this.excludedFilter.forEach((el) => delete newQueryParam[el]);
 
     this.query = this.query.find(
       JSON.parse(
-        JSON.stringify(queryStrCopy).replaceAll(
+        JSON.stringify(newQueryParam).replaceAll(
           /\b (gt|gte|lt|lte)\b/g,
           (match) => '$' + match
         )
@@ -22,26 +22,26 @@ module.exports = class ApiFeatures {
     return this;
   }
   paginate() {
-    const page = +this.queryStr.page || 1;
-    const limit = +this.queryStr.limit || 10;
+    const page = +this.queryParams.page || 1;
+    const limit = +this.queryParams.limit || 10;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
     return this;
   }
   sort() {
-    if (this.queryStr.sort) {
-      let sortBy = this.queryStr.sort;
+    if (this.queryParams.sort) {
+      let sortBy = this.queryParams.sort;
       sortBy = sortBy.split(',').join(' ');
-      this.query.sort(sortBy);
+      this.query = this.query.sort(sortBy);
     } else this.query = this.query.sort('-createdAt');
     return this;
   }
   limitFields() {
-    if (this.queryStr.limit) {
-      let limitBy = this.queryStr.limit;
+    if (this.queryParams.limit) {
+      let limitBy = this.queryParams.limit;
       limitBy = limitBy.split(',').join(' ');
-      this.query.select(limitBy);
+      this.query = this.query.select(limitBy);
     } else this.query = this.query.select('-__v');
     return this;
   }
