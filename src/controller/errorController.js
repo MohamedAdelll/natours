11345b1efe59ca17cef03f4f66d46a78df0f1eb1
Mohamed Ashour <97407829+MohamedAdelll@users.noreply.github.com>
2024-11-23 18:28:40 +0200

    implementeing booking controller

--- src/controller/errorController.js
+++ src/controller/errorController.js
@@ -82,9 +82,7 @@ const sendErrorProd = (err, req, res) => {
   });
 };
 
-module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
+module.exports = (err, req, res) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
